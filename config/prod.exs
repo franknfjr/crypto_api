ab6bf627212ebc0d08ecc066bd2c2da57cf198d0
franknfjr <frankdonascimentojr@gmail.com>
2022-04-16 23:31:28 -0300

    test

--- config/prod.exs
+++ config/prod.exs
@@ -18,7 +18,7 @@ config :logger, level: :info
 
 config :crypto_api, CryptoApi.Repo,
   database: "crypto-api",
-  socket: "/cloudsql/tests-333512:us-central1:coins-elixir"
+  socket: "/cloudsql/cryptoapi-347423:us-central1:cryptodb/.s.PGSQL.5432"
 
 # ## SSL Support
 #
